218d617b9d681a4504f6debe64cb40a386051dad
Viren Mody <virenmody@gmail.com>
2018-04-11 11:54:28 -0500

    added functionality for q5 and allow only 2 to be checked

--- app/src/main/java/com/example/android/hamiltonquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/hamiltonquizapp/MainActivity.java
@@ -8,14 +8,21 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static int numQuestions = 4;
+    private static int numQuestions = 5;
+    private Queue<CheckBox> boxesCheckedQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boxesCheckedQueue = new LinkedList<CheckBox>();
     }
 
     /**
@@ -59,13 +66,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * The method returns the result of question #5
+     * @return 1 if question was correct, 0 if incorrect
+     */
+    private int getQ5Result(){
+
+        if(((CheckBox) findViewById(R.id.q5_answer_hamilton_check1)).isChecked() &&
+                ((CheckBox) findViewById(R.id.q5_answer_jefferson_check3)).isChecked() &&
+                !((CheckBox) findViewById(R.id.q5_answer_washington_check2)).isChecked() &&
+                !((CheckBox) findViewById(R.id.q5_answer_burr_check4)).isChecked()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * This method will allow only two checkboxes to be checked at a time. When a 3rd is checked, the 1st checked will be unchecked.
+     * @param view is the checkbox that was checked or unchecked
+     */
+    public void allowOnlyTwo(View view) {
+
+        CheckBox checkBox = (CheckBox) view;
+
+        // If this checkbox is checked, add to the queue
+        if(checkBox.isChecked()) {
+
+            // If 2 are checked, uncheck the box that was 1st checked of the 2
+            if(boxesCheckedQueue.size() == 2) {
+                CheckBox firstChecked = boxesCheckedQueue.poll();
+                firstChecked.setChecked(false);
+            }
+            // Add new checked box onto queue
+            boxesCheckedQueue.add((CheckBox) checkBox);
+        }
+        // If this checkbox is unchecked, remove it from the queue
+        else {
+            boxesCheckedQueue.remove(checkBox);
+        }
+
+    }
+
+    /**
      * This method is called when the Submit button is clicked
      * @param view
      */
     public void gradeQuiz (View view) {
         int numCorrect = 0;
 
-        numCorrect += getQ1Result() + getQ2Result() + getQ3Result() + getQ4Result();
+        numCorrect += getQ1Result() + getQ2Result() + getQ3Result() + getQ4Result() + getQ5Result();
         String toastScoreMessage = "You got " + numCorrect + " out of " + numQuestions + " correct.";
 
         // Show a toast with the number correct out of the number of questions
